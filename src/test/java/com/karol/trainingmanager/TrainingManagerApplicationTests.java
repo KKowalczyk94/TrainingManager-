7f8e2642494b7c6d1247832ff7fce612eaacfc72
KKowalczyk94 <karolxkow@gmail.com>
2023-05-19 18:34:06 +0200

    #1 setup maven project

---
+++ src/test/java/com/karol/trainingmanager/TrainingManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karol.trainingmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
